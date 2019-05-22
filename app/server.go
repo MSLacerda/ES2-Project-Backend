e9b2e48966f80dc2db60f34ab7ed49ba7a986f08
MSLacerda <the.matheus6@gmail.com>
2019-05-22 17:30:14 -0300

    fix(casos, cors): fix server

--- app/server.go
+++ app/server.go
@@ -5,11 +5,19 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/rs/cors"
+
 	"github.com/MSLacerda/ES2-Project-Backend/app/router"
 )
 
 func BuiltServer() {
 	port := os.Getenv("PORT")
+	// corsObj := handlers.AllowedOrigins([]string{"*"})
+	// methodsObj := handlers.AllowedMethods([]string{"POST", "PUT", "GET", "DELETE", "OPTIONS"})
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},                                       // All origins
+		AllowedMethods: []string{"GET", "OPTIONS", "POST", "DELETE", "PUT"}, // Allowing only get, just an example
+	})
 
 	if port == "" {
 		port = "8080"
@@ -17,5 +25,5 @@ func BuiltServer() {
 
 	route := router.GetRouter()
 
-	log.Fatal(http.ListenAndServe(":"+port, route))
+	log.Fatal(http.ListenAndServe(":"+port, c.Handler(route)))
 }
